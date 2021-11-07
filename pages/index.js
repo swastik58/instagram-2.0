fc1487e3e2b1436729a63e4d3467f033619dfebd
Swastik Supakar <swastiksupakar19@gmail.com>
2021-11-07 23:56:21 +0530

    code upload

--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,7 @@ import Head from 'next/head'
 import Feed from '../components/Feed'
 import Header from '../components/Header'
 import Modal from '../components/Modal'
+import { Layout, Typography, Space } from 'antd';
 
 export default function Home() {
   return (
@@ -18,6 +19,14 @@ export default function Home() {
 
         {/*modal*/}
         <Modal />
+        <br /><br />
+        <div className="bg-gray-200 items-center text-center text-black italic">
+                    <Typography.Title level={5} style={{color: 'black', textAlign: 'center', fontSize: '25px'}}>
+                        INSTAGRAM -- 2.0 <br /> 
+                        A frontend Instagram website clone made by <a className="text-blue-900" href="https://swastik-portfolio.netlify.app/" target="_blank" rel="noreferrer">Swastik </a><br />
+                    </Typography.Title>
+                    <p className="not-italic font-bold text-blue-800"><a href="https://github.com/swastik58/instagram-2.0" target="_blank" rel="noreferrer">GitHub Source Code</a></p>
+        </div>
     </div>
   )
 }
